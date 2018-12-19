0edf3230bb87d9b24060300c4bd4b07069801597
Gurpreet Deol <deol.gs@hotmail.com>
2018-12-19 15:13:33 -0800

    pages authentication module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './pages/authentication/authentication.component';
+import { ChatListComponent } from './pages/chat-list/chat-list.component';
+import { ConversationComponent } from './pages/conversation/conversation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    ChatListComponent,
+    ConversationComponent,
+    HomeComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
